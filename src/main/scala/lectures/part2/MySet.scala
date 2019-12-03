3c61fcd0172266d369f1ad84df5b119abb136563
David Pereira <dpal.pereira@campus.fct.unl.pt>
2019-12-03 12:04:13 +0000

    PropertyBasedSet

--- src/main/scala/lectures/part2/MySet.scala
+++ src/main/scala/lectures/part2/MySet.scala
@@ -1,5 +1,7 @@
 package lectures.part2
 
+import scala.annotation.tailrec
+
 trait MySet[A] extends (A => Boolean) {
 
   def apply(elem : A): Boolean = contains(elem)
@@ -31,10 +33,10 @@ class EmptySet[A] extends MySet[A] {
   override def -(elem: A): MySet[A] = this
   override def --(anotherSet: MySet[A]): MySet[A] = this
   override def &(anotherSet: MySet[A]): MySet[A] = this
-  override def unary_! : MySet[A] = new AllInclusiveSet[A]
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](_=>true)
 }
 
-class AllInclusiveSet[A] extends MySet[A] {
+/*class AllInclusiveSet[A] extends MySet[A] {
   override def contains(elem: A): Boolean = true
   override def +(elem: A): MySet[A] = this
   override def ++(anotherSet: MySet[A]): MySet[A] = this
@@ -48,10 +50,28 @@ class AllInclusiveSet[A] extends MySet[A] {
   override def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
   override def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
   override def unary_! : MySet[A] = new EmptySet[A]
+}*/
+
+//all elements of type A which satisfy a property
+class PropertyBasedSet[A](property: A=> Boolean) extends MySet[A] {
+  override def contains(elem: A): Boolean = property(elem)
+  override def +(elem: A): MySet[A] =
+    new PropertyBasedSet[A](x => property(x) || x == elem)
+  override def ++(anotherSet: MySet[A]): MySet[A] =
+    new PropertyBasedSet[A](x => property(x) || x == anotherSet(x))
+  override def map[B](f: A => B): MySet[B] = politelyFail
+  override def flatMap[B](f: A => MySet[B]): MySet[B] = politelyFail
+  override def forEach(e: A => Unit): Unit = politelyFail
+  override def filter(predicate: A => Boolean): MySet[A] = new PropertyBasedSet[A](x => property(x) && predicate(x))
+  override def -(elem: A): MySet[A] = filter(_!=elem)
+  override def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+  override def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](x => !property(x))
+
+  def politelyFail = throw new IllegalArgumentException("Really complicated problem =(")
 }
 
 class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
-
   override def contains(elem: A): Boolean = elem == head || tail.contains(elem)
   override def +(elem: A): MySet[A] = if (this contains elem) this else new NonEmptySet[A](elem, this)
   override def ++(anotherSet: MySet[A]): MySet[A] = tail ++ anotherSet + head
@@ -66,5 +86,24 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
   override def -(elem: A): MySet[A] = if(head == elem) tail else tail - elem + head
   override def --(anotherSet: MySet[A]): MySet[A] = filter(x => !anotherSet.contains(x))
   override def &(anotherSet: MySet[A]): MySet[A] = filter(x => anotherSet.contains(x))
-  override def unary_! : MySet[A] = ???
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](x => this.contains(x))
+}
+
+object MySet {
+  def apply[A](values: A*): MySet[A] = {
+    @tailrec
+    def buildSet(velSeq: Seq[A], acc: MySet[A]): MySet[A] =
+      if(velSeq.isEmpty) acc
+      else buildSet(velSeq.tail, acc + velSeq.head)
+    buildSet(values.toSeq, new EmptySet[A])
+  }
+}
+
+object RunningCode extends App {
+  val s = MySet(1,2,3,4)
+  val negative  = !s
+
+  println(negative(2))
+  println(negative(5))
+  println{(negative + 5)(5)}
 }
